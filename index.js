999f0fa037b0ec14d7831c3d4fd383d3a105c8ed
yoolha <127286924+yoolha@users.noreply.github.com>
2023-12-19 11:43:30 +0900

    m8

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-// const express = require('express')   // common JS
-import express from 'express'          // ES6
+// index.js
 
-const app = express()
-const port = 3000
+import express from 'express';
+import { tempRouter } from './src/routes/temp.route.js';
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+const app = express();
+const port = 3000;
+
+// router setting
+app.use('/temp', tempRouter);
+
+// error handling
+app.use((err, req, res, next) => {
+    // 템플릿 엔진 변수 설정
+    res.locals.message = err.message;   
+    // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; 
+    res.status(err.data.status).send(response(err.data));
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+		console.log(`Example app listening on port ${port}`);
+});
