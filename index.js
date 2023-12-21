9db5046a240563ad559dce5eedffd56a79ce1360
yoolha <127286924+yoolha@users.noreply.github.com>
2023-12-22 00:12:18 +0900

    9주차

--- index.js
+++ index.js
@@ -2,12 +2,36 @@
 
 import express from 'express';
 import { tempRouter } from './src/routes/temp.route.js';
+import { userRouter } from './src/routes/user.route.js';
+import { specs } from './swagger/swagger.config.js';
+import SwaggerUi from 'swagger-ui-express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();    // .env 파일 사용 (환경 변수 관리)
 
 const app = express();
 const port = 3000;
 
+// server setting - veiw, static, body-parser etc..
+app.set('port', process.env.PORT || 3000)   // 서버 포트 지정
+app.use(cors());                            // cors 방식 허용
+app.use(express.static('public'));          // 정적 파일 접근
+app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
+app.use(express.urlencoded({extended: false})); // 단순 객체 문자열 형태로 본문 데이터 해석
+app.use('/user', userRouter);
+// (...)
+
+
+
+// swagger
+app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(specs));
+app.use(express.urlencoded({extended: false})); // 단순 객체 문자열 형태로 본문 데이터 해석
+
+
 // router setting
 app.use('/temp', tempRouter);
+app.use('/user', userRouter);
 
 // error handling
 app.use((err, req, res, next) => {
@@ -15,9 +39,10 @@ app.use((err, req, res, next) => {
     res.locals.message = err.message;   
     // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; 
-    res.status(err.data.status).send(response(err.data));
+    console.log("error", err);
+    res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
 });
 
-app.listen(port, () => {
-		console.log(`Example app listening on port ${port}`);
+app.listen(app.get('port'), () => {
+    console.log(`Example app listening on port ${app.get('port')}`);
 });
